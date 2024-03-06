699654d23b96d3ffec1a24286a4753cb52721aee
Ripcord595 <Yujie.Lu@uqtr.ca>
2024-03-06 18:48:45 -0500

    add homepage to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component'; // 确保正确导入
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent }, // 将homepage设为根路由
+  // ...其他路由
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
