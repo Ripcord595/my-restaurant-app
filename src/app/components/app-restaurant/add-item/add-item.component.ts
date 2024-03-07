b5891d2273b246059f877060cc83cec9874be33c
guanting <hegting1379@gmail.com>
2024-03-06 22:26:27 -0500

    popup window

---
+++ src/app/components/app-restaurant/add-item/add-item.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item',
+  templateUrl: './add-item.component.html',
+  styleUrl: './add-item.component.css'
+})
+export class AddItemComponent {
+  selectedImage: any;
+
+  // être appelée lorsque la valeur de sélecteur du fichier est changée
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  // Logique de upload
+  onUpload(): void {
+  }
+}
