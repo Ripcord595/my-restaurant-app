671d70f89c9e3019ddd5dd8b603dd33bbf8b402d
Ripcord595 <Yujie.Lu@uqtr.ca>
2024-03-06 19:01:57 -0500

    little update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 // src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomepageComponent } from './components/homepage/homepage.component'; // 确保正确导入
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }, // 将homepage设为根路由
-  // ...其他路由
+  { path: '', component: HomepageComponent }, // Définir la page d'accueil comme itinéraire racine
+
 ];
 
 @NgModule({
