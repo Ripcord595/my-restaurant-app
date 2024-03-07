e9a2f4daa270114e269d8b27975f5ffdfabdfaa2
Ripcord595 <Yujie.Lu@uqtr.ca>
2024-03-06 19:42:56 -0500

    little update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import {LoginComponent} from "./components/login/login.component";
+import {SignupComponent} from "./components/signup/signup.component";
+
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }, // Définir la page d'accueil comme itinéraire racine
-
+  { path: 'homepage', component: HomepageComponent }, // Définir la page d'accueil comme itinéraire racine
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
